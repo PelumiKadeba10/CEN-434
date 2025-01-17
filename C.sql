551791522755a9ee380ed55fc69592026ee747d5
PelumiKadeba10 <92647600+PelumiKadeba10@users.noreply.github.com>
2025-01-17 08:10:28 +0100

    Add files via upload

---
+++ C.sql
@@ -0,0 +1,17 @@
+CREATE TABLE Students (
+    ID INT,
+    Name VARCHAR(50),
+    MatriculationNumber VARCHAR(20),
+    Gender VARCHAR(7) 
+);
+INSERT INTO Students (ID, Name, MatriculationNumber, Gender) 
+VALUES (200380, 'Cleo', '20CM000001', 'Female'),
+       (210008, 'Loras', '21CJ00002', 'Male'),
+       (210004, 'James', '21CJ00003', 'Male'),
+       (210006, 'Dora', '21CJ00004', 'Male'),
+       (110009, 'Pelumi', '11CJ00005', 'Male'),
+       (200002, 'Ygritte', '20CJ00006', 'Female'),
+       (290008, 'Pott', '18Ck00007', 'Male'),
+       (240013, 'Levi', '27CJ00008', 'Male'),
+       (240006, 'Dorment', '20CJ00009', 'Female'),
+       (150009, 'Tom', '17CJ00010', 'Male');
